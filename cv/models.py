66a9b0dc5a126ae88482e0470b552b6e52c2a588
liu yuenfu <liuyuenfu@gmail.com>
2021-12-18 17:51:49 +0100

    created models and registered it in admin

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    first_name = models.CharField(max_length=100,null=False, help_text="please enter your own name")
+    last_name = models.CharField(max_length=100,null=False, help_text="please enter your father's name")
+    
+    def __str__(self):
+        return f"{self.last_name.upper()} {self.first_name}"
